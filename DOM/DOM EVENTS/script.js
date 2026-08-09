cb91c519655e6df1ee0c2c4077cc2dd0ac7f471a
Ankit SIngh <maddens3702h@gmail.com>
2026-08-09 23:02:37 +0530

    form

--- DOM/DOM EVENTS/script.js
+++ DOM/DOM EVENTS/script.js
@@ -4,10 +4,11 @@ const surname = document.querySelector("#surname");
 
 form.addEventListener("submit", (events)=>{
     events.preventDefault();
-//    console.log(name.value);
-//    console.log(surname.value);
-      console.log(events.target[0].value);
-      console.log(events.target[1].value);
-      console.log(events.target[2].value);
-   
-})
+
+    let naam = name.value;
+    let surnaam = surname.value;
+
+    console.log(naam, surnaam);
+    
+    form.reset();
+});
