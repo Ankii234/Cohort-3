36f5f3b1a8f2c0b8a6d12434f7685451bd33309c
Ankit SIngh <maddens3702h@gmail.com>
2026-08-10 23:36:55 +0530

    good nyt

--- DOM/DOM EVENTS/script.js
+++ DOM/DOM EVENTS/script.js
@@ -65,6 +65,7 @@ form.addEventListener("submit", (events)=>{
     <img src="https://images.unsplash.com/photo-1786130995704-30b4863426a2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D" alt="img from unsplash">
     </div>
     <div class="text">
+    
     <h2>Name - ${name} </h2>
     <p>Email - ${email} </p>
     </div>
