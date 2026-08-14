71665864d45c6d0ad5a9fabe6eb06b9f4119ac92
Ankit SIngh <maddens3702h@gmail.com>
2026-08-14 22:46:37 +0530

    hh

--- DOM/DOM EVENTS/script.js
+++ DOM/DOM EVENTS/script.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form");
 const inp1 = document.querySelector("#name");
 const inp2 = document.querySelector("#email");
 const users = document.querySelector(".users");
+const url = document.querySelector("#url");
 
 
 let usersData = [
@@ -59,10 +60,11 @@ form.addEventListener("submit", (events)=>{
 
     let name = inp1.value;
     let email = inp2.value;
+    let imageUrl = url.value;
     if (name.trim() === "" && email.trim() === "") return;
     users.innerHTML += `<div class="user_card">
     <div class="img_box">
-    <img src="https://images.unsplash.com/photo-1786130995704-30b4863426a2?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D" alt="img from unsplash">
+    <img src=" ${imageUrl} ">
     </div>
     <div class="text">
     
