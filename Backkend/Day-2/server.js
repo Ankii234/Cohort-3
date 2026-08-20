ad8b0268fa0ad57037c840bc9630050a749da783
Ankit SIngh <maddens3702h@gmail.com>
2026-08-21 00:06:02 +0530

    b-1

--- Backkend/Day-2/server.js
+++ Backkend/Day-2/server.js
@@ -11,14 +11,17 @@
     
 // })
 
-const express = require("express");
+// const express = require("express");
 
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("ok got it")
+// });
+
+// app.listen(3000,() => {
+//     console.log("server is running on port 3000");
+// })
+
+const express = require("express")
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("ok got it")
-});
-
-app.listen(3000,() => {
-    console.log("server is running on port 3000");
-})
