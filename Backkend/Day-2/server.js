6ca9e20b07e2f6535d9c4ce36459ff9726e3fbc1
Ankit SIngh <maddens3702h@gmail.com>
2026-08-20 14:02:26 +0530

    Backend started

---
+++ Backkend/Day-2/server.js
@@ -0,0 +1,25 @@
+// let http = require("http");
+
+// let server = http.createServer((req, res) => {
+//   console.log("hey...");
+//   res.end("ok get it")
+  
+// });
+
+// server.listen(3000, ()=>{
+//     console.log("Started");
+    
+// })
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("ok got it")
+});
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+    
+})
