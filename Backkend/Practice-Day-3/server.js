970d3dc186fe5dd296d9b6765400eb0404661664
Ankit SIngh <maddens3702h@gmail.com>
2026-08-21 08:50:01 +0530

    postman day 1

--- Backkend/Practice-Day-3/server.js
+++ Backkend/Practice-Day-3/server.js
@@ -13,17 +13,35 @@
 
 const express = require("express");
 let app = express();
+app.use(express.json());
+const users = [];
+let nextId= 1;
 
+// to read the get api 
 app.get("/",(req, res) => {
-    res.send("Server start kro jarvis")
+    res.json(users)
 });
+
+// For creation of post api 
+app.post("/create",(req, res) => {
+    const {name, age} = req.body;
+    const user = {id:nextId++, name, age}
+    users.push(user);
+
+    res.send("Created Succesfully")
+})
+
+app.put("/update",(req, res) =>{
+
+})
+
+app.delete("/delete/:id",(req, res) =>{
+     let id = req.params.id;
+     users.filter((value) => value.id!== id)
+     res.send("User Deleted")
+})
  
-// res.end("Hey")
-
-const http = require("http")
-
-
 app.listen(3000, () => {
-    console.log(http);
+    console.log("Server jud-chuka hai");
     
 });
