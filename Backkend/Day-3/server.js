1478471485bfebbe12278f22577b2808e14f4c12
Ankit SIngh <maddens3702h@gmail.com>
2026-08-22 00:46:31 +0530

    crud

--- Backkend/Day-3/server.js
+++ Backkend/Day-3/server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 
 let port = 3000;
+
+let users = [
+    {
+        name: "pol",
+        age: 21,
+        gender: "male"
+    },
+];
+
+app.get("/",(req, res) => {
+     res.send(users)
+
+});
+
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
     
-})
+});
