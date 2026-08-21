91b76f9d409618e2e682613ce14945a874556cfa
Ankit SIngh <maddens3702h@gmail.com>
2026-08-21 07:43:16 +0530

    ii

--- Backkend/Practice-Day-3/server.js
+++ Backkend/Practice-Day-3/server.js
@@ -1,12 +1,29 @@
+// const http = require("http")
+
+// const server = http.createServer((req,res) =>{
+//    console.log("server started");
+//    res.end("Hey");
+   
+// })
+
+// server.listen(3000, () =>{
+//    console.log("App is listed");
+   
+// })
+
+const express = require("express");
+let app = express();
+
+app.get("/",(req, res) => {
+    res.send("Server start kro jarvis")
+});
+ 
+// res.end("Hey")
+
 const http = require("http")
 
-const server = http.createServer((req,res) =>{
-   console.log("server started");
-   res.end("Hey");
-   
-})
 
-server.listen(3000, () =>{
-   console.log("App is listed");
-   
-})
+app.listen(3000, () => {
+    console.log(http);
+    
+});
