0a45c2504e266182949214664f021a4af7c1b708
Ankit SIngh <maddens3702h@gmail.com>
2026-09-01 01:13:31 +0530

    login4

--- Practice/Login/src/LoginForm.jsx
+++ Practice/Login/src/LoginForm.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const LoginForm = () => {
   return (
-
+    
     <div>
-      {/* html here  */}
+      {/* html here */}
       <form>
-      <input type="email" placeholder="Enter Email">
-      <br>
-      <br>
-      <input type="password" placeholder="Enter Password">
-      <br>
-      <br>
-      <button type="submit">Login</button>
-    </form>
+        <input type="email" placeholder="Enter Email" />
+        <br />
+        <br />
+        <input type="password" placeholder="Enter Password" />
+        <br />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
 
-export default LoginForm
+export default LoginForm;
