cb72817c02f40a0f46d81f81194231a9a59625f5
Ankit SIngh <maddens3702h@gmail.com>
2026-09-02 00:02:21 +0530

    evnt last

--- DOM/DOM-EVNT-DELEGATION/script.js
+++ DOM/DOM-EVNT-DELEGATION/script.js
@@ -1,4 +1,4 @@
-const main = document.querySelector("main");
+// const main = document.querySelector("main");
 
 // main.addEventListener("click", (e)=>{
 //    console.log(e.target)
@@ -7,3 +7,66 @@ const main = document.querySelector("main");
 // let random = Math.random() * 3;
 // Math.floor(random);
 // console.log(random);
+
+const main = document.querySelector("main");
+
+const btn = document.querySelector("button");
+const timer = document.querySelector("#timer");
+const scoore = document.querySelector("#score");
+const overlay = document.querySelector("#overlay");
+
+const box = document.createElement('div')
+box.classList.add('box')
+
+let time = 0;
+let interval;
+let score = 0;
+
+
+const randomColor = () =>{
+ let r = Math.floor(Math.random() * 256);
+ let g = Math.floor(Math.random() * 256);
+ let b = Math.floor(Math.random() * 256);
+
+ return `rgb(${r}, ${b}, ${g})`;
+}
+
+
+const randomBox = () =>{
+   box.style.backgroundColor = randomColor();
+   main.append(box);
+
+   mainH = main.clientHeight - box.offsetHeight;
+   mainW = main.clientWidth - box.offsetWidth;
+   
+   const rY = Math.random() * mainH;
+   const rX = Math.random() * mainW;
+
+   box.style.top = `${rY}px`;
+   box.style.left = `${rX}px`;
+}
+
+
+btn.addEventListener('click', () =>{
+   main.append(box);
+   clearInterval(interval)
+
+   interval = setInterval(() => {
+     randomBox();
+     time +=1;
+
+   timer.textContent= time;
+   }, 1000);
+
+   setTimeout(() => {
+      clearInterval(interval);
+      overlay.style.display = "flex";
+   }, 10000);
+})
+
+box.addEventListener('click', ()=>{
+   score +=1;
+   scoore.textContent = score;
+})
+
+
