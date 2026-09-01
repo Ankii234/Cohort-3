2790b436e744f3d843bb3fb34dca4ced642d1f64
Ankit SIngh <maddens3702h@gmail.com>
2026-09-01 20:15:49 +0530

    dom event

--- DOM/DOM-EVNT-DELEGATION/script.js
+++ DOM/DOM-EVNT-DELEGATION/script.js
@@ -1,5 +1,9 @@
 const main = document.querySelector("main");
 
-main.addEventListener("click", (e)=>{
-   console.log(e.target)
-});
+// main.addEventListener("click", (e)=>{
+//    console.log(e.target)
+// });
+
+// let random = Math.random() * 3;
+// Math.floor(random);
+// console.log(random);
