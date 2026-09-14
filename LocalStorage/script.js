5c818177f965ed5894d0188e4088fb496c65c881
Ankit SIngh <maddens3702h@gmail.com>
2026-09-14 10:18:15 +0530

    lcl2

---
+++ LocalStorage/script.js
@@ -0,0 +1,8 @@
+// Button click pe
+document.body.classList.toggle("dark");
+localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
+
+// Page load pe
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
